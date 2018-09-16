72d235bd3d1d387a2090f048ae2693f9d362bebe
Christian <christianmonte@gmail.com>
2018-09-16 14:53:18 -0500

    add babel

--- src/index.js
+++ src/index.js
@@ -1,40 +1,43 @@
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 function responseMiddleware() {
   const service = 'service';
   const success = 'success';
   const failure = 'failure';
-
-  return () => next => (action) => {
+  return () => next => action => {
     if (Object.prototype.hasOwnProperty.call(action, service)) {
-      if (typeof (action[service]) === 'object') {
-        const data = `${action.target}Data`;
-        const error = `${action.target}Error`;
-        const loading = `${action.target}Loading`;
-
+      if (typeof action[service] === 'object') {
+        const data = "".concat(action.target, "Data");
+        const error = "".concat(action.target, "Error");
+        const loading = "".concat(action.target, "Loading");
         const payload = {
           [data]: false,
           [error]: false,
-          [loading]: false,
+          [loading]: false
         };
-
-        next({
-          ...action,
-          payload: { ...payload, [loading]: true },
-        });
-
-        action[service].then((resp) => {
-          next({
-            ...action,
-            payload: { ...payload, [data]: action.response(resp) },
-          });
+        next(_objectSpread({}, action, {
+          payload: _objectSpread({}, payload, {
+            [loading]: true
+          })
+        }));
+        action[service].then(resp => {
+          next(_objectSpread({}, action, {
+            payload: _objectSpread({}, payload, {
+              [data]: action.response(resp)
+            })
+          }));
 
           if (Object.prototype.hasOwnProperty.call(action, success)) {
             action[success]();
           }
-        }).catch((err) => {
-          next({
-            ...action,
-            payload: { ...payload, [error]: err },
-          });
+        }).catch(err => {
+          next(_objectSpread({}, action, {
+            payload: _objectSpread({}, payload, {
+              [error]: err
+            })
+          }));
 
           if (Object.prototype.hasOwnProperty.call(action, failure)) {
             action[failure]();
